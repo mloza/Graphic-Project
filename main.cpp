04de66289f7f5b8caa780159f4e7def60bc62d29
Michał Łoza <scorneds@gmail.com>
2012-01-04 22:09:20 +0100

    Specyfikacja

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+struct colors {
+    void RGBToYUV() {}
+    void RGBToHSV() {}
+    void RGBToHSL() {}
+
+    void YUVToRGB() {}
+    void HSVToRGB() {}
+    void HSLToRGB() {}
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
