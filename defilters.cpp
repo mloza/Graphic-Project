2187d49ebd5385b8c3494670106c3038e2ecacd5
Michał Łoza <scorneds@gmail.com>
2012-01-21 16:26:47 +0100

    Dzialajacy filtr roznicowy - male poprawki

--- defilters.cpp
+++ defilters.cpp
@@ -9,14 +9,11 @@ namespace defilter {
     bool differential(std::vector<unsigned char> &data, int width, int height)
     {
         int akt;
-        ofstream f("out/aft.txt");
-        f << (int)data[0] << endl;
         for(int j=1; j<width*height*3; j++)
         {
             akt = data[j] + data[j-1];
             if(akt > 255) akt %= 256;
             data[j] = akt;
-            f << (int)data[j] << endl;
         }
         return true;
     }
